a3bc4c60ea711c2a2198185a31ddb077afb62e4c
second <wuwenbosecond@163.com>
2017-03-07 22:13:00 +0800

    change link

--- first_demo/pages/home/home.js
+++ first_demo/pages/home/home.js
@@ -6,10 +6,10 @@ Page({
     },
     onLoad: function (options) {
         // 生命周期函数--监听页面加载
-        this.setData({rec_key:postsData.postcons});
-        this.setData({icons_key:postsData.posticons});
-        this.setData({banners_key:postsData.posbanners});
-},
+        this.setData({ rec_key: postsData.postcons });
+        this.setData({ icons_key: postsData.posticons });
+        this.setData({ banners_key: postsData.posbanners });
+    },
     onShareAppMessage: function () {
         // 用户点击右上角分享
         return {
@@ -18,9 +18,14 @@ Page({
             path: 'path' // 分享路径
         }
     },
-    onTap:function(){
-      wx.navigateTo({
-        url: "/pages/contentlist/contentlist"
-      });
+    onTap: function () {
+        wx.navigateTo({
+            url: "/pages/contentlist/contentlist"
+        });
+    },
+    recTap: function () {
+        wx.navigateTo({
+            url: "/pages/contentdetail/contentdetail"
+        });
     }
 })
