1674544da768ee5090ea36a63adb483de2369f34
second <wuwenbosecond@163.com>
2017-05-14 10:32:29 +0800

    下拉刷新

--- first_demo/pages/contentlist/contentlist.js
+++ first_demo/pages/contentlist/contentlist.js
@@ -1,37 +1,75 @@
 
 Page({
-    data: {
+  data: {
+    allcontent: {},
+    is_empty: true,
+    contentcount: 0,
+    content_key:{},
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    var titlename = options.iconname;
+    var dataid = options.typeid;
+    this.setData({ dataid_key: dataid });
+    this.getdata(dataid, titlename);
 
-    },
-    onLoad: function (options) {
-        // 生命周期函数--监听页面加载
-        var titlename = options.iconname;
-        var dataid = options.typeid;
-        this.getdata(dataid,titlename);
-
-    },
-    getdata: function (dataid,titlename) {
-        var that = this;
-        wx.request({
-            url: 'http://192.168.142.128:8000/api/content/' + dataid + '/contentbytype/',
-            data: {},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: {
-                "content-Type": "json"
-            }, // 设置请求的 header
-            success: function (res) {
-                that.setData({ content_key: res.data });
-                that.setData({ title_key: titlename });
-            },
-            fail: function () {
-                console.log("fail")
-            }
-        })
-    },
-    onContentid: function (event) {
-        var contentid = event.currentTarget.dataset.contentid;
-        wx.navigateTo({
-            url: "/pages/contentdetail/contentdetail?id=" + contentid
-        })
+  },
+  getdata: function (dataid, titlename) {
+    var that = this;
+    wx.request({
+      url: 'http://192.168.142.128:8000/api/content/' + dataid + '/contentbytype/?page=0',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "content-Type": "json"
+      }, // 设置请求的 header
+      success: function (res) {
+        that.setData({ content_key: res.data });
+        that.setData({ title_key: titlename });
+        that.setData({ is_empty: false });
+        that.setData({ contentcount: 1 });
+      },
+      fail: function () {
+        console.log("fail");
+      }
+    })
+  },
+  onContentid: function (event) {
+    var contentid = event.currentTarget.dataset.contentid;
+    wx.navigateTo({
+      url: "/pages/contentdetail/contentdetail?id=" + contentid
+    })
+  },
+  scrollupper: function (event) {
+    var that = this;
+    var count = this.data.contentcount;
+    var empty = this.data.is_empty;
+    var content = this.data.content_key;
+    console.log(content);
+    if (empty) {
+      console.log("加载错误")
     }
+    else {
+      wx.request({
+        url: 'http://192.168.142.128:8000/api/content/' + this.data.dataid_key + '/contentbytype/?page=' + count,
+        data: {},
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECTd
+        header: {
+          "content-Type": "json"
+        }, // 设置请求的 header
+        success: function (res) {
+          content = content.concat(res.data);
+          that.setData({ content_key: content});
+          that.setData({ isempty: false });
+          count = count+1;
+          that.setData({ contentcount : count});
+        },
+        fail: function () {
+          console.log("fail")
+        }
+      })
+    };
+
+  },
+
 })
--- first_demo/pages/home/home.js
+++ first_demo/pages/home/home.js
@@ -1,67 +1,97 @@
 // var postsData = require('../../data/data.js')
 
 Page({
-    data: {
-
-    },
-    onLoad: function (options) {
-        // 生命周期函数--监听页面加载
-        this.getrec();
-        this.geticons();
-    },
-    onShareAppMessage: function () {
-        // 用户点击右上角分享
-        return {
-            title: 'title', // 分享标题
-            desc: 'desc', // 分享描述
-            path: 'path' // 分享路径
-        }
-    },
-
-    getrec: function (event) {
-        var that = this;
-        wx.request({
-            url: 'http://192.168.142.128:8000/api/content/REC/recimfor/',
-            data: {},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: { "content-Type": "json" }, // 设置请求的 header
-            success: function (res) {
-                that.setData({ rec_key: res.data });
-            },
-        })
-    },
-
-    geticons: function (event) {
-        var that = this;
-        wx.request({
-            url: 'http://192.168.142.128:8000/api/icon/',
-            data: {},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: { "content-Type": "json" }, // 设置请求的 header
-            success: function (res) {
-                 that.setData({ icons_key: res.data });
-            },
-        })
-    },
-
-    bannertap: function (event) {
-        var contentid = event.currentTarget.dataset.contentid;
-        wx.navigateTo({
-            url: "/pages/contentdetail/contentdetail?id=" + contentid
-        });
-    },
-    onTap: function (event) {
-        var labelid = event.currentTarget.dataset.labelid;
-        var icon_name = event.currentTarget.dataset.iconname;
-        wx.navigateTo({
-            url: "/pages/contentlist/contentlist?typeid=" + labelid + "&iconname=" + icon_name
-        });
-    },
-    recTap: function (event) {
-        var contentid = event.currentTarget.dataset.contentid;
-        var recimfor = event.currentTarget.dataset.recimfor;
-        wx.navigateTo({
-            url: "/pages/contentdetail/contentdetail?id=" + contentid + "&recimfor=" + recimfor
-        });
+  data: {
+    count: 0,
+    is_empty: true,
+    rec_key: {},
+    banner_key: {},
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    this.getrec();
+    this.geticons();
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
     }
+  },
+
+  getrec: function (event) {
+    var that = this;
+    wx.request({
+      url: 'http://192.168.142.128:8000/api/content/REC/recimfor/?page=0',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: { "content-Type": "json" }, // 设置请求的 header
+      success: function (res) {
+        that.setData({ rec_key: res.data });
+        that.setData({ banner_key: res.data });
+        that.setData({ count: 1 });
+        that.setData({ is_empty: false });
+      },
+    })
+  },
+
+  geticons: function (event) {
+    var that = this;
+    wx.request({
+      url: 'http://192.168.142.128:8000/api/icon/',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: { "content-Type": "json" }, // 设置请求的 header
+      success: function (res) {
+        that.setData({ icons_key: res.data });
+      },
+    })
+  },
+
+  bannertap: function (event) {
+    var contentid = event.currentTarget.dataset.contentid;
+    wx.navigateTo({
+      url: "/pages/contentdetail/contentdetail?id=" + contentid
+    });
+  },
+  onTap: function (event) {
+    var labelid = event.currentTarget.dataset.labelid;
+    var icon_name = event.currentTarget.dataset.iconname;
+    wx.navigateTo({
+      url: "/pages/contentlist/contentlist?typeid=" + labelid + "&iconname=" + icon_name
+    });
+  },
+  recTap: function (event) {
+    var contentid = event.currentTarget.dataset.contentid;
+    var recimfor = event.currentTarget.dataset.recimfor;
+    wx.navigateTo({
+      url: "/pages/contentdetail/contentdetail?id=" + contentid + "&recimfor=" + recimfor
+    });
+  },
+  scrollupper: function (event) {
+    var that = this;
+    var reccon = this.data.rec_key;
+    var rec_count = this.data.count
+    var empty = this.data.is_empty;
+    if (empty) {
+      console.log("又加载错了，初始没有获取到内容，不要再下拉了")
+    }
+    else {
+      wx.request({
+        url: 'http://192.168.142.128:8000/api/content/REC/recimfor/?page=' + rec_count,
+        data: {},
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        header: { "content-Type": "json" }, // 设置请求的 header
+        success: function (res) {
+          rec_count = rec_count + 1;
+          reccon = reccon.concat(res.data);
+          that.setData({ rec_key: reccon });
+          that.setData({ count: rec_count});
+        },
+      })
+    };
+    
+  }
 })
